d4b4939fccbd5701d0271251165da566a4b27bf5
ergegananputra <126530940+ergegananputra@users.noreply.github.com>
2024-05-17 15:40:12 +0700

    Update mwcc.sql

--- mwcc.sql
+++ mwcc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2024 at 10:34 AM
+-- Generation Time: May 17, 2024 at 10:39 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
