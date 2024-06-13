edb95bd609aec0661ebd74fc90c6a68718c7e7d4
ergegananputra <126530940+ergegananputra@users.noreply.github.com>
2024-06-13 18:04:17 +0700

    fix: kajian route in admin panel

--- resources/views/kajian/write/form_editor_admin.blade.php
+++ resources/views/kajian/write/form_editor_admin.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.layout_admin')
 
 @section('style')
+    <!-- Summernote Lite WYSIWYG (Erge 30 Mei 2024)-->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
     <style>
         .note-editor.fullscreen {
             background-color: white
@@ -93,4 +97,22 @@
                      
         </div>
     </main>
+
+        <!-- Summernote Lite -->
+        <script>
+            $('#editor').summernote({
+            placeholder: 'Ketik disini',
+            tabsize: 2,
+            height: 500,
+            toolbar: [
+                ['style', ['style']],
+                ['font', ['bold', 'underline', 'clear']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['table', ['table']],
+                ['insert', ['link', 'picture', 'video']],
+                ['view', ['fullscreen', 'codeview', 'help']]
+            ]
+            });
+        </script>
 @endsection
